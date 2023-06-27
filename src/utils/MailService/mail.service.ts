4c88613bf62fb7694dccaaa37a3610f2b69fa787
ivanChri <ics82717@gmail.com>
2023-06-27 22:41:48 +0700

    changes MailService provider from Nodemiler to Emailjs

--- src/utils/MailService/mail.service.ts
+++ src/utils/MailService/mail.service.ts
@@ -3,50 +3,30 @@
 /* eslint-disable prettier/prettier */
 import { Injectable } from "@nestjs/common/decorators/core/injectable.decorator";
 import { ConfigService } from "@nestjs/config/dist";
-import * as nodemailer from "nodemailer";
+const fetch = require("node-fetch")
 @Injectable()
 export class MailService {
   constructor(private config:ConfigService){}
     async sendApiKey(receiver:string,key:string,token:string){
-      const transporter = nodemailer.createTransport({
-     service: this.config.get("SERVICE"),
-      host:this.config.get("HOST"),
-      port:587,
-      tls: {
-        ciphers:this.config.get("CIPHERS"),
-        rejectUnauthorized: false,
-      },
-      auth: {
-        user: this.config.get("USER"),
-        pass: this.config.get("PASSWORD")
-      },
-      });
-      await new Promise((resolve, reject) => {
-        // verify connection configuration
-        transporter.verify( (error, success) => {
-            if (error) {
-                console.log(error);
-                reject(error);
-            } else {
-                console.log("Server is ready to take our messages");
-                resolve(success);
-            }
-        });
-    });
-      const mailOptions = {
-        from:this.config.get("USER"),
-        to:receiver,
-        subject:"apiKey",
-        text:`your apikey is ${key} and  access_token: ${token}`
+      const data = {
+        service_id : this.config.get("SERVICE"),
+        template_id : this.config.get("TEMPLATE_ID"),
+        user_id: this.config.get("USER_ID"),
+        accessToken :this.config.get("ACCESS_TOKEN"),
+        template_params: {
+          to_name:receiver,
+          from_name:this.config.get("USER"),
+          message:`your apiKey is ${key} and your acces_token ${token}`
+        }
       };
-    await new Promise((resolve, reject) => {
-        transporter.sendMail(mailOptions,(error, response) => {
-              if (error) {
-                  reject(error)
-              } else {
-                resolve(response)
-              }
-          });
-    })
+       try {
+        return await fetch(this.config.get("SERVICE_URL"), {
+          method: 'POST',
+          body: JSON.stringify(data),
+          headers: {'Content-Type': 'application/json'}
+         });
+       } catch (error) {
+         throw error;
+       }
     };
 };
